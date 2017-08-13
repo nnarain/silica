e99b040578620a8d2d810611abab7936770ee032
Natesh Narain <nnaraindev@gmail.com>
2017-08-13 00:18:45 -0400

    return a Result from assemble with data

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,7 +1,11 @@
 mod lexer;
 
+use std::io::Error;
+
 /// consume input data and assemble the code
-pub fn assemble(input_data: Vec<u8>) {
+pub fn assemble(input_data: Vec<u8>) -> Result<Vec<u8>, Error> {
     let tokens = lexer::tokenize(&input_data[..]).unwrap();
     println!("{:?}", tokens);
+
+    Ok(vec![])
 }
