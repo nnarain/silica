3ad8b82f29b1e36470f76aa157750967c3ce848c
Natesh Narain <nnaraindev@gmail.com>
2017-08-18 17:33:42 -0400

    Added parsers for labels and directives expressions

--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -4,27 +4,43 @@ use std::io::Error;
 use nom::*;
 
 /// an expression is a certain combination of tokens
-type Expression = Vec<Token>;
+pub type Expression = Vec<Token>;
 
-fn take_token(tokens: &[Token]) -> IResult<&[Token], &[Token]> {
-    if tokens.len() > 0 {
-        match token[0] {
-            Token::Label(_) => {
-                IResult::Done(&tokens[1..], &tokens[0..1])
-            },
-            _ => {
-                IResult::Error()
+macro_rules! tag_token {
+    ($i: expr, $tag: pat) => (
+        {
+            if $i.len() > 0 {
+                match $i[0] {
+                    $tag => {
+                        IResult::Done(&$i[1..], $i[0].clone())
+                    },
+                    _ => {
+                        IResult::Error(error_position!(ErrorKind::Tag, $i))
+                    }
+                }
+            }
+            else {
+                IResult::Incomplete(Needed::Size(1))
             }
         }
-    }
-    else {
-        IResult::Incomplete(Needed::Size(1))
-    }
+    )
 }
 
 /// parse labels from tokens
 named!(parse_label<&[Token], Expression>,
-    
+    do_parse!(
+        label: tag_token!(Token::Label(_)) >>
+        (vec![label])
+    )    
+);
+
+/// parse directive
+named!(parse_directive<&[Token], Expression>,
+    do_parse!(
+        directive: tag_token!(Token::Directive(_)) >>
+        num: tag_token!(Token::NumericLiteral(_)) >>
+        (vec![directive, num])
+    )
 );
 
 /// parse expressions from tokens
@@ -34,5 +50,23 @@ pub fn parse_expressions(tokens: Vec<Token>) -> Result<Vec<Expression>, Error> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    fn test_parse_label() {
+        let input = vec![Token::Label(String::from("JP"))];
+        let result = parse_label(&input[..]);
+        let empty: Vec<Token> = vec![];
+
+        assert_eq!(result, IResult::Done(&empty[..], vec![Token::Label(String::from("JP"))]));
+    }
+
+    #[test]
+    fn test_parse_directive() {
+        let input = vec![Token::Directive(String::from("org")), Token::NumericLiteral(0x200)];
+        let result = parse_directive(&input[..]);
+        let empty: Vec<Token> = vec![];
+
+        assert_eq!(result, IResult::Done(&empty[..], vec![Token::Directive(String::from("org")), Token::NumericLiteral(0x200)]));
+    }
 }
