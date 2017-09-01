8d0a23075ffae6ef09537c73f735a01ff4b65df4
Natesh Narain <nnaraindev@gmail.com>
2017-09-01 16:11:13 -0400

    Added the semantics checker to the assemble function

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -10,7 +10,12 @@ pub fn assemble(input_data: Vec<u8>) -> Result<Vec<u8>, Error> {
     // transform input data into tokens
     let tokens = lexer::tokenize(&input_data[..]).unwrap();
     // transform tokens into expressions
-    let exprs = parser::parse(tokens);
+    let exprs = parser::parse(tokens).unwrap();
+
+    // iterate over the expressions, check if they are valid and pass the code generator
+    for expr in exprs.iter() {
+        semantics::check(expr).unwrap();
+    }
 
     Ok(vec![])
 }
